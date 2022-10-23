3b2ac471fd031c81cf38a8f2da7318450815dc5c
professor91 <keshavcode3@gmail.com>
2022-10-24 00:25:38 +0530

    deleted unnecessary files

--- global_vars.py
+++
@@ -1,9 +0,0 @@
-# global vars 
-from log import CreateLogger
-from json_db import jsdb
-
-FileManager= jsdb("database/fileSystem.json")
-
-db24hour_logger= CreateLogger(FileManager._cacheddata["logs"][0])
-dbtest_logger= CreateLogger(FileManager._cacheddata["logs"][2])
-mytokens_logger= CreateLogger(FileManager._cacheddata["logs"][1])
